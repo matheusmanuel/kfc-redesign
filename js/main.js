d5e7cc2d4e08ace6b93972715ae626a6b1c65e26
Matheus Manuel <matheusmanuel111@gmail.com>
2023-05-29 16:01:27 +0200

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// slide
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 10,
+    spaceBetween: 10,
+  });
